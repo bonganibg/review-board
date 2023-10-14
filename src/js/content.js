e336db05883f72f3d10332f5b31b74051b4f1b9c
Bonganibg <bongani.gumbo@yahoo.com>
2023-10-14 16:55:27 +0200

    updated a few things

--- src/js/content.js
+++ src/js/content.js
@@ -1,16 +1,16 @@
-import { WarningController } from "../controllers/warning.controller.js";
-import { ThreadController } from "../controllers/thread.controller.js";
+// import { WarningController } from "../controllers/warning.controller.js";
+// import { ThreadController } from "../controllers/thread.controller.js";
 
-// Shared Variables 
-const STUDENT_NUMBER = "STU18027103";
-const REVIEWER_NAME = "Jimmy"
+// // Shared Variables 
+// const STUDENT_NUMBER = "STU18027103";
+// const REVIEWER_NAME = "Jimmy"
 
-// UI components
-const contentScreen = document.getElementById("contentScreen");
-const btnThread = document.getElementById("btnThreads");
-const btnEfficiency = document.getElementById("btnEfficiency");
-const btnStyle = document.getElementById("btnStyle");
-const btnDocumentation = document.getElementById("btnDocumentation");
+// // UI components
+// const contentScreen = document.getElementById("contentScreen");
+// const btnThread = document.getElementById("btnThreads");
+// const btnEfficiency = document.getElementById("btnEfficiency");
+// const btnStyle = document.getElementById("btnStyle");
+// const btnDocumentation = document.getElementById("btnDocumentation");
 
 // Services 
 // const threadController = new ThreadController(STUDENT_NUMBER, REVIEWER_NAME, contentScreen);
@@ -18,24 +18,23 @@ const btnDocumentation = document.getElementById("btnDocumentation");
 // const warningController = new WarningController(STUDENT_NUMBER, REVIEWER_NAME, "style", contentScreen);
 // warningController.loadPage();
 
-btnThread.addEventListener('click', () => {    
-    let threadController = new ThreadController(STUDENT_NUMBER, REVIEWER_NAME, contentScreen);
-    threadController.loadPage();
-});
+// btnThread.addEventListener('click', () => {    
+//     let threadController = new ThreadController(STUDENT_NUMBER, REVIEWER_NAME, contentScreen);
+//     threadController.loadPage();
+// });
 
-btnEfficiency.addEventListener('click', () => {
-    let warningController = new WarningController(STUDENT_NUMBER, REVIEWER_NAME, "efficiency", contentScreen);
-    warningController.loadPage();
-});
+// btnEfficiency.addEventListener('click', () => {
+//     let warningController = new WarningController(STUDENT_NUMBER, REVIEWER_NAME, "efficiency", contentScreen);
+//     warningController.loadPage();
+// });
 
-btnStyle.addEventListener('click', () => {
-    let warningController = new WarningController(STUDENT_NUMBER, REVIEWER_NAME, "style", contentScreen);
-    warningController.loadPage();
+// btnStyle.addEventListener('click', () => {
+//     let warningController = new WarningController(STUDENT_NUMBER, REVIEWER_NAME, "style", contentScreen);
+//     warningController.loadPage();
 
-});
+// });
 
-btnDocumentation.addEventListener('click', () => {
-    let warningController = new WarningController(STUDENT_NUMBER, REVIEWER_NAME, "documentation", contentScreen);
-    warningController.loadPage();
-});
-
+// btnDocumentation.addEventListener('click', () => {
+//     let warningController = new WarningController(STUDENT_NUMBER, REVIEWER_NAME, "documentation", contentScreen);
+//     warningController.loadPage();
+// });
