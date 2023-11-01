d2106eeb6c270ea33b3934717e29c84c672100cb
bonganibg <bonganibg@proton.me>
2023-11-01 15:05:31 +0200

    hacky messaging

--- src/background.js
+++ src/background.js
@@ -2,8 +2,9 @@ import { WarningController } from "./controllers/warning.controller.js";
 import { ThreadController } from "./controllers/thread.controller.js";
 
 // Shared Variables 
-const STUDENT_NUMBER = "STU18027103";
-const REVIEWER_NAME = "Jimmy"
+const STUDENT_NUMBER = undefined;
+const REVIEWER_NAME = undefined;
+const STUDENT_NAME = undefined
 
 // UI components
 const contentScreen = document.getElementById("contentScreen");
@@ -12,38 +13,38 @@ const btnEfficiency = document.getElementById("btnEfficiency");
 const btnStyle = document.getElementById("btnStyle");
 const btnDocumentation = document.getElementById("btnDocumentation");
 
-const testing = document.getElementById("testing")
-
-
-
+const setup = false;
 
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
-  console.log('We in here')
-  // Validate the message's structure.
-  if ((msg.from === 'content')) {
-    testing.innerText = msg.subject;
+  console.log(msg)
+  if ((msg.source === 'content' && setup == false)) {
+    STUDENT_NUMBER = msg.studentNumber;
+    REVIEWER_NAME = msg.reviewer;
+    STUDENT_NAME = msg.studentName;
+
+    btnThread.textContent = STUDENT_NAME;
+    setup = true;
+
+    btnThread.addEventListener('click', () => {    
+      let threadController = new ThreadController(STUDENT_NUMBER, REVIEWER_NAME, contentScreen);
+      threadController.loadPage();
+    });
+    
+    btnEfficiency.addEventListener('click', () => {
+        let warningController = new WarningController(STUDENT_NUMBER, REVIEWER_NAME, "efficiency", contentScreen);
+        warningController.loadPage();
+    });
+    
+    btnStyle.addEventListener('click', () => {
+        let warningController = new WarningController(STUDENT_NUMBER, REVIEWER_NAME, "style", contentScreen);
+        warningController.loadPage();
+    
+    });
+    
+    btnDocumentation.addEventListener('click', () => {
+        let warningController = new WarningController(STUDENT_NUMBER, REVIEWER_NAME, "documentation", contentScreen);
+        warningController.loadPage();
+    });
+
   }
 });
-
-
-
-// btnThread.addEventListener('click', () => {    
-//     let threadController = new ThreadController(STUDENT_NUMBER, REVIEWER_NAME, contentScreen);
-//     threadController.loadPage();
-// });
-
-// btnEfficiency.addEventListener('click', () => {
-//     let warningController = new WarningController(STUDENT_NUMBER, REVIEWER_NAME, "efficiency", contentScreen);
-//     warningController.loadPage();
-// });
-
-// btnStyle.addEventListener('click', () => {
-//     let warningController = new WarningController(STUDENT_NUMBER, REVIEWER_NAME, "style", contentScreen);
-//     warningController.loadPage();
-
-// });
-
-// btnDocumentation.addEventListener('click', () => {
-//     let warningController = new WarningController(STUDENT_NUMBER, REVIEWER_NAME, "documentation", contentScreen);
-//     warningController.loadPage();
-// });
--- src/content.js
+++ src/content.js
@@ -3,15 +3,16 @@ const studentDetails = document.getElementsByClassName("student-block")[0].getEl
 const studentNumber = studentDetails[3].textContent.replace("Student number:","").trim()
 const studentName = studentDetails[0].textContent.replace("Student: ","").trim()
 
-// while ()
+const details = {
+  source: "content",
+  reviewer: reviewer,
+  studentNumber: studentNumber,
+  studentName: studentName
+}
 
 // When the popup is activated, setup the listener
-function SetMessage()
-{
-  chrome.runtime.sendMessage({
-    from: 'content',
-    subject: 'showPageAction',
-  });
+function SetMessage(){
+  chrome.runtime.sendMessage(details);
 }
   
 setInterval(SetMessage, 1000)
